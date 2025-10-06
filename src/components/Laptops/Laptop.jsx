05d1d80c4873f1b23009e954b84931eed5d866c6
mjh-shikder <mjhsikder@gmail.com>
2025-10-06 21:47:36 +0600

    adding multiple children

---
+++ src/components/Laptops/Laptop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Laptop = () => {
+    return (
+        <div>
+            <h2>THis is laptopaaaaaas</h2>
+        </div>
+    );
+};
+
+export default Laptop;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Mobiles from './components/Mobiles/Mobiles.jsx';
+import Laptop from './components/Laptops/Laptop.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello from react router</div>,
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: 'mobiles', Component: Mobiles },
+      {path: 'laptops', Component: Laptop}
+    ]
   },
   {
     path: 'about',
@@ -18,6 +27,10 @@ const router = createBrowserRouter([
   {
     path: 'blogs',
     element: <div>All my blogs are here</div>
+  },
+  {
+    path: 'app',
+    Component: App //element: <App></App>
   }
 ]);
 
